15166ee701da5043d618be8c7f628c2eb69faf78
Jesus Noland <jesus.noland@litpen.com>
2023-07-07 06:47:39 -0700

    Added an exit for the game

--- guessing.py
+++ guessing.py
@@ -7,7 +7,7 @@ def get_num_from_player() -> int:
     number_to_guess = -1
     while True:
         try:
-            number_to_guess = int(input("User 1, enter a number between 0 and 100: "))
+            number_to_guess = int(input("Enter a number between 0 and 100: "))
             if number_to_guess <= 0 or number_to_guess >= 100:
                 print("Try again")
                 continue
@@ -18,11 +18,30 @@ def get_num_from_player() -> int:
         break
     return number_to_guess
 
-if __name__ == '__main__':
-    num_to_guess = random.randrange(0, 100)
-    user_guess = get_num_from_player()
 
-    if num_to_guess == user_guess:
-        print("You guessed the number!")
-    else:
-        print("Incorrect")
+def get_continue_from_player() -> bool:
+    player_will_continue = False
+    try:
+        player_input = input("Continue playing? (y / n): ")
+        if player_input == "y" or player_input == "yes" or player_input == "Y":
+            player_will_continue = True
+    except ValueError:
+        pass
+
+    return player_will_continue
+
+
+if __name__ == '__main__':
+    while True:
+        num_to_guess = random.randrange(0, 100)
+        user_guess = get_num_from_player()
+
+        if num_to_guess == user_guess:
+            print("You guessed the number!")
+        else:
+            print("Incorrect")
+        
+        if get_continue_from_player():
+            continue
+        else:
+            break
